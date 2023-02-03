38bc2d0fd79b35b531ef3f757f45272272a8b421
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-02-03 11:07:07 +0200

    add checkmark for shopping-list
    Checkmark for ingredient list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { RouletteComponent } from './roulette/roulette.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { RouletteComponent } from './roulette/roulette.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
