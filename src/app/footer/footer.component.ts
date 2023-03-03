10d4372d474d1f8cdae2dbde5f729dea2eff34c4
Suvikan <sanledell@gmail.com>
2023-03-03 11:47:22 +0200

    add fortawesome brand icons and edit footer
    team members need to install "npm install @fortawesome/free-brands-svg-icons" to their node modules for brand icons to work

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faTwitter = faTwitter;
 
 }
