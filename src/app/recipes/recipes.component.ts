8c76030dbe4b4901eb7b2a6408fb809475de3d56
vhlahti <henri.veholahti@gmail.com>
2023-03-03 14:12:56 +0200

    add roulette filtering and random function

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
-import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-recipes',
@@ -8,23 +7,12 @@ import { DataService } from '../data.service';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+
+  @Input() chosenRecipe: Observable<any[]>;
+  
   showShoppingList = false;
 
-  recipes: Observable<any>;
-
-  constructor(private dataService: DataService) {}
-
-  ngOnInit(): void {
-    this.getRecipes();
-  }
-
-  getRecipes(): void {
-    this.recipes = this.dataService.getAllRecipes();
-    (error) => {
-      console.log('http-error:');
-      console.log(error);
-    };
-  }
+  ngOnInit(): void {}
 
   onShowShoppingList() {
     this.showShoppingList = !this.showShoppingList;
--- src/app/roulette/roulette.component.ts
+++ src/app/roulette/roulette.component.ts
@@ -1,60 +1,131 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import recipeData from 'src/assets/json/testi.json';
-
-interface Recipe {
-  id: Number;
-  name: String; 
-  ruokavalio: String;
-  ohje: String;
-  valmistusaika: String;
-  ainekset: String[];
-}
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-roulette',
   templateUrl: './roulette.component.html',
   styleUrls: ['./roulette.component.css']
 })
-export class RouletteComponent {
+export class RouletteComponent implements OnInit {
+
+  recipes: Observable<any>;
+  randomId: number;
+  chosenRecipe: Observable<any[]>;
 
   // form handler
 
   rouletteForm: FormGroup;
 
-  constructor(public fb: FormBuilder) {
+  constructor(private dataService: DataService,
+              public fb: FormBuilder) {
     this.rouletteForm = fb.group({
       roulette: ['', Validators.required]
     });
   }
 
+  // get all recipes
+
+  ngOnInit(): void {
+    this.getRecipes();
+  }
+
+  getRecipes(): void {
+    this.recipes = this.dataService.getAllRecipes();
+    (error) => {
+      console.log('http-error:');
+      console.log(error);
+    };
+  }
+
   // button handler
+  // the function filters the recipes and extracts the chosen content into chosenRecipe array
 
   onSubmit() {
   console.log(this.rouletteForm.value);
 
   if (this.rouletteForm.value.roulette === 'meat')
   {
-    console.log('Liha!');
+
+    // first the function looks through the recipes and creates a randomId from the id pool
+
+    this.recipes.pipe(
+      map(items => items.filter(item => item.id.toString().startsWith("1"))),
+      map(filteredItems => {
+        const randomIndex = Math.floor(Math.random() * filteredItems.length);
+        return filteredItems[randomIndex].id;
+      })
+    ).subscribe(randomId => {
+      this.randomId = randomId;
+      console.log(randomId);
+
+    // then it extracts the referenced id object (recipe) and puts it into chosenRecipe
+
+    this.chosenRecipe = this.recipes.pipe(
+      map(items => items.filter(item => item.id === randomId))
+    );
+
+    // subscribe to the chosenRecipe observable to retrieve the recipe
+
+    this.chosenRecipe.subscribe(recipes => console.log(recipes));
+
+  });
   }
   if (this.rouletteForm.value.roulette === 'fish')
   {
-    console.log('Kala!');
+
+    this.recipes.pipe(
+      map(items => items.filter(item => item.id.toString().startsWith("2"))),
+      map(filteredItems => {
+        const randomIndex = Math.floor(Math.random() * filteredItems.length);
+        return filteredItems[randomIndex].id;
+      })
+    ).subscribe(randomId => {
+      this.randomId = randomId;
+      console.log(randomId);
+      this.chosenRecipe = this.recipes.pipe(
+        map(items => items.filter(item => item.id === randomId))
+      );
+      this.chosenRecipe.subscribe(recipes => console.log(recipes));
+    });
   }
   if (this.rouletteForm.value.roulette === 'vege')
   {
-    console.log('Vege!');
+
+    this.recipes.pipe(
+      map(items => items.filter(item => item.id.toString().startsWith("3"))),
+      map(filteredItems => {
+        const randomIndex = Math.floor(Math.random() * filteredItems.length);
+        return filteredItems[randomIndex].id;
+      })
+    ).subscribe(randomId => {
+      this.randomId = randomId;
+      console.log(randomId);
+      this.chosenRecipe = this.recipes.pipe(
+        map(items => items.filter(item => item.id === randomId))
+      );
+      this.chosenRecipe.subscribe(recipes => console.log(recipes));
+    });
   }
   if (this.rouletteForm.value.roulette === 'vegan')
   {
-    console.log('Vegan!');
+
+    this.recipes.pipe(
+      map(items => items.filter(item => item.id.toString().startsWith("4"))),
+      map(filteredItems => {
+        const randomIndex = Math.floor(Math.random() * filteredItems.length);
+        return filteredItems[randomIndex].id;
+      })
+    ).subscribe(randomId => {
+      this.randomId = randomId;
+      console.log(randomId);
+      this.chosenRecipe = this.recipes.pipe(
+        map(items => items.filter(item => item.id === randomId))
+      );
+      this.chosenRecipe.subscribe(recipes => console.log(recipes));
+    });
   }
   }
-
-  // recipe data
-
-  recipes: Recipe[] = recipeData;
-  //lisää filtteri ruokavalion mukaan
-  //random
-  selected: Recipe = this.recipes[0]; 
 }
